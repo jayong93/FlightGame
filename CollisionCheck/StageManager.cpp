73647f6adca6e8ddb4f4e0e17b6961e6a6be7aa2
jayong93 <jayong93@gmail.com>
2015-12-08 00:32:28 +0900

    새로운 맵 데이터 형식 적용
    링 추가 버전 맵데이터

--- CollisionCheck/StageManager.cpp
+++ CollisionCheck/StageManager.cpp
@@ -37,10 +37,12 @@ void StageManager::Init()
 	for (size_t i = 0; i < data.size(); ++i)
 	{
 		Json::Value& value = data[i];
-		float height = rand() % 250 + 200;
-		Building building(vec3(value["w"].asInt() / 2.0f, height / 2.0f, value["h"].asInt() / 2.0f), vec3(value["x"].asInt() - mapW / 2, height / 2.0, -(value["y"].asInt() - mapH / 2)), vec3(value["w"].asInt() / 2.0f, height / 2.0f, value["h"].asInt() / 2.0f).GetSize(), 0.0f, (float)(rand() % 360), 0.0f);
-		building.SetColor(rand() / (float)RAND_MAX * 2 + 0.4f, rand() / (float)RAND_MAX * 2 + 0.4f, rand() / (float)RAND_MAX * 2 + 0.4f);
-		buildingList.push_back(building);
+		if (value["type"].asString() == "building")
+		{
+			Building building(vec3(value["w"].asInt() / 2.0f, value["d"].asInt() / 2.0f, value["h"].asInt() / 2.0f), vec3(value["x"].asInt() - mapW / 2, value["d"].asInt() / 2.0, -(value["y"].asInt() - mapH / 2)), vec3(value["w"].asInt() / 2.0f, value["d"].asInt() / 2.0f, value["h"].asInt() / 2.0f).GetSize(), 0.0f, (float)(rand() % 360), 0.0f);
+			building.SetColor(rand() / (float)RAND_MAX * 2 + 0.4f, rand() / (float)RAND_MAX * 2 + 0.4f, rand() / (float)RAND_MAX * 2 + 0.4f);
+			buildingList.push_back(building);
+		}
 	}
 
 	FILE* file;
@@ -73,26 +75,4 @@ void StageManager::Init()
 void StageManager::Render()
 {
 	for (unsigned int i = 0; i < buildingList.size(); ++i) buildingList[i].Render();
-
-	//int d[8][2] = { { 1, 0 },{ 1, 1 },{ 0, 1 },{ -1, 1 },{ -1, 0 },{ -1, -1 },{ 0, -1 },{ 1, -1 } };
-
-	//for (int i = 0; i < 32; ++i) {
-	//	for (int j = 0; j < 32; ++j) {
-	//		glPushMatrix();
-	//		glColor3f(1.0f, 0.6f, 0.6f);
-	//		glTranslatef((387.1f*i) - 6000, 0.0f, (387.1f*j) - 6000.0f);
-	//		for (int k = 0; k < 8; ++k) {
-	//			if (node[j][i] & (1 << k)) {
-	//				int nx = d[k][0], nz = d[k][1];
-	//				if (nx + i < 0 || nx + i >= 32 || nz + j < 0 || nz >= 32) continue;
-	//				glBegin(GL_LINES); {
-	//					glVertex3f(0.0f, 0.0f, 0.0f);
-	//					glVertex3f(387.1f*nx, 0.0f, 387.1f*nz);
-	//				}glEnd();
-	//			}
-	//		}
-	//		glutSolidSphere(30.0f, 32, 32);
-	//		glPopMatrix();
-	//	}
-	//}
 }
