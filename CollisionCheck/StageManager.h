1652b8735eba6bd8925fde414785d7f29b216aac
jayong93 <jayong93@gmail.com>
2015-12-12 21:55:19 +0900

    쿼드트리, 유도탄

--- CollisionCheck/StageManager.h
+++ CollisionCheck/StageManager.h
@@ -2,15 +2,41 @@
 #include "Object.h"
 #include <vector>
 
+class Bullet;
 
 class StageManager {
+	struct Rect {
+		vec3 vPoint1, vPoint2;
+
+		bool isLeafNode;
+		std::vector<Building> pObjectList;
+
+		Rect() : vPoint1(), vPoint2(), isLeafNode(false) {	}
+
+		bool Isin(vec3& p1, vec3& p2) {
+			if (vPoint1.x < p2.x && p1.x < vPoint2.x
+				&&vPoint1.z < p2.z && p1.z < vPoint2.z) {
+				return(true);
+			}
+			return(false);
+		}
+		void Draw() {
+			if (isLeafNode) {
+				for (int i = 0; i<pObjectList.size(); ++i) {
+					pObjectList[i].Render();
+				}
+			}
+		}
+	};
+	Rect quadTree[85];
+
 	static StageManager* instance;
 	int nodeNum;
 
 	unsigned char** node;
 	float mapW, mapH;
 
-	std::vector<Building> buildingList;
+	//std::vector<Building> buildingList;
 	std::vector<Object*> objectList;
 
 	StageManager();
@@ -19,7 +45,7 @@ public:
 
 	static StageManager* Instance();
 
-	std::vector<Building>* GetBuildingList() { return(&buildingList); }
+	std::vector<std::vector<Building>*> GetBuildingList(float x, float z, float radius);
 
 	void Init();
 
@@ -32,4 +58,6 @@ public:
 	unsigned char GetNodeDate(int row, int col);
 
 	bool GetAStarRoute(Node& start, Node& destiny, std::vector<Node>& routeList);
+
+	void CollisonCheck_Bullet(std::vector<Bullet>* bulletList);
 };
